f2fd49fa95e553aa52caa0aec24d988ef18b72ae
Brian Dietrick <brian_dietrick@hotmail.com>
2022-03-27 20:30:40 -0400

    initial skeleton

--- app.py
+++ app.py
@@ -1,12 +1,40 @@
-from flask import Flask
+from flask import Flask, render_template, jsonify, json, request, redirect, url_for
+import os
+import util
 
 app = Flask(__name__)
 
 
-@app.route('/')
-def hello_world():  # put application's code here
-    return 'Hello World!'
+@app.route('/', methods=['GET'])
+def home():
+    return render_template('home.html')
 
+@app.route('/items', methods=['GET'])
+def get_items():
+    return render_template('items.html')
+
+@app.route('/item', methods=['GET'])
+def get_item():
+    return render_template('item.html')
+
+@app.route('/createitem', methods=['GET','POST'])
+def create_item():
+    return render_template('createitem.html')
+
+@app.route('/articles', methods=['GET'])
+def get_articles():
+    return render_template('articles.html')
+
+@app.route('/article', methods=['GET'])
+def get_article():
+    return render_template('article.html')
+
+@app.route('/createarticle', methods=['GET','POST'])
+def create_article():
+    return render_template('createarticle.html')
 
 if __name__ == '__main__':
-    app.run()
+    app.debug = True
+    ip = '127.0.0.1'
+    port = 5000
+    app.run(host=ip, port=port)
---
+++ static/js/main.js
@@ -0,0 +1,37 @@
+$(document).ready(function(){
+	// $('#display_button').click(function(){
+		// $.get( "/api/get_team", function( data ) {
+		  // // clean display paragraph
+		  // $('#display_p').text('');
+		  // var js_obj = JSON.parse(data);
+
+		  // $.each(js_obj['all_teams'], function (index, entry) {
+		  	// // console.log(index);
+		  	// $('#display_p').append($("<span></span><br>").text(entry.team_name)); 
+		  // });
+		 
+		// });
+	// });
+	// $('#schedule_button').click(function(){
+
+		// $.get( "/api/generate_presentation_order", function( data ) {
+		  // // clean display paragraph
+		  // $('#display_p').text('');
+
+		  // var js_obj = JSON.parse(data);
+		  // $('#display_p').append($("<span></span><br>").text('First Day:'));
+		  // $.each(js_obj['first_day'], function (index, entry) {
+		  	// // console.log(index);
+		  	// $('#display_p').append($("<span></span><br>").text(entry.team.team_name+', '+entry.start_time));
+		    
+		  // });
+
+		  // $('#display_p').append($("<span></span><br>").text('Second Day:'));
+		  // $.each(js_obj['second_day'], function (index, entry) {
+		  	// // console.log(index);
+		  	// $('#display_p').append($("<span></span><br>").text(entry.team.team_name+', '+entry.start_time));
+		    
+		  // });
+		// });
+	// });
+});
